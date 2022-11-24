fbe20602761df121365d07a15d23445844f30745
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-24 01:09:59 +0000

    update ProductDetails.test.js

--- src/containers/tests/ProductDetails.test.js
+++ src/containers/tests/ProductDetails.test.js
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import ProductDetails from '../ProductDetails';
 import store from '../../redux/store';
-import { BrowserRouter } from 'react-router-dom';
 
 describe('ProductDetails page test', () => {
   test('Render ProductDetails page', () => {
@@ -10,9 +10,10 @@ describe('ProductDetails page test', () => {
       <BrowserRouter>
         <Provider store={store}>
           <ProductDetails />
-        </Provider>,
-      </BrowserRouter>  
-      
+        </Provider>
+        ,
+      </BrowserRouter>,
+
     );
     expect(view).toMatchSnapshot();
   });
