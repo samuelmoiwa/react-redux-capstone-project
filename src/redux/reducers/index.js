7d7487735f1e8cf497ae27e08bd5f710d0d64b87
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-24 01:11:10 +0000

    update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from "redux";
-import { productsReducer, selectedProductsReducer } from "./productsReducer";
+import { combineReducers } from 'redux';
+import { productsReducer, selectedProductsReducer } from './productsReducer';
 
 const reducers = combineReducers({
   allProducts: productsReducer,
