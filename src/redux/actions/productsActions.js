189dc24027432e8b8b23967197c0c54fb5194d02
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-24 01:10:36 +0000

    update products Actions.js

--- src/redux/actions/productsActions.js
+++ src/redux/actions/productsActions.js
@@ -1,20 +1,15 @@
-import { ActionTypes } from "../constants/action-types";
+/* eslint-disable import/named */
+import { ActionTypes } from '../constants/action-types';
 
-export const setProducts = (products) => {
-  return {
-    type: ActionTypes.SET_PRODUCTS,
-    payload: products,
-  };
-};
+export const setProducts = (products) => ({
+  type: ActionTypes.SET_PRODUCTS,
+  payload: products,
+});
 
-export const selectedProduct = (product) => {
-  return {
-    type: ActionTypes.SELECTED_PRODUCT,
-    payload: product,
-  };
-};
-export const removeSelectedProduct = () => {
-  return {
-    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
-  };
-};
+export const selectedProduct = (product) => ({
+  type: ActionTypes.SELECTED_PRODUCT,
+  payload: product,
+});
+export const removeSelectedProduct = () => ({
+  type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+});
