da08ca6a8a314d4e96e48e5adca1e2709378674f
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-24 01:08:30 +0000

    update productDetails.js

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,29 +1,32 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { selectedProduct, removeSelectedProduct, } from "../redux/actions/productsActions";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectedProduct, removeSelectedProduct } from '../redux/actions/productsActions';
 
 const URL = 'https://fakestoreapi.com/products/';
 
 const ProductDetails = () => {
   const { productId } = useParams();
-  let product = useSelector((state) => state.product);
-  const { image, title, price, category, description } = product;
+  const product = useSelector((state) => state.product);
+  const {
+    image, title, price, category, description,
+  } = product;
   const dispatch = useDispatch();
   const fetchProductDetail = async (id) => {
     const response = await axios
-      .get(URL+id)
+      .get(URL + id)
       .catch((err) => {
-        console.log("Err: ", err);
+        console.log('Err: ', err);
       });
     dispatch(selectedProduct(response.data));
   };
 
   useEffect(() => {
-    if (productId && productId !== "") fetchProductDetail(productId);
+    if (productId && productId !== '') fetchProductDetail(productId);
     return () => {
       dispatch(removeSelectedProduct());
     };
@@ -39,18 +42,21 @@ const ProductDetails = () => {
             <div className="ui vertical divider">AND</div>
             <div className="middle aligned row">
               <div className="column lp">
-                <img className="ui fluid image" src={image} alt="product"/>
+                <img className="ui fluid image" src={image} alt="product" />
               </div>
               <div className="column rp">
                 <h1>{title}</h1>
                 <h2>
-                  <a className="ui teal tag label">${price}</a>
+                  <a className="ui teal tag label">
+                    $
+                    {price}
+                  </a>
                 </h2>
                 <h3 className="ui brown block header">{category}</h3>
                 <p>{description}</p>
                 <div className="ui vertical animated button" tabIndex="0">
                   <div className="hidden content">
-                    <i className="shop icon"></i>
+                    <i className="shop icon" />
                   </div>
                   <div className="visible content">Add to Cart</div>
                 </div>
