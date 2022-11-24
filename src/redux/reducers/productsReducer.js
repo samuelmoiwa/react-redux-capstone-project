28716245320cc3adf9657fa9153ecf81580483f7
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-11-24 01:05:51 +0000

    update productsReducer.js

--- src/redux/reducers/productsReducer.js
+++ src/redux/reducers/productsReducer.js
@@ -1,4 +1,6 @@
-import { ActionTypes } from "../constants/action-types";
+/* eslint-disable import/named */
+import ActionTypes from '../constants/action-types';
+
 const intialState = {
   products: [],
 };
@@ -13,7 +15,6 @@ export const productsReducer = (state = intialState, { type, payload }) => {
 };
 
 export const selectedProductsReducer = (state = {}, { type, payload }) => {
-  console.log(type);
   switch (type) {
     case ActionTypes.SELECTED_PRODUCT:
       return { ...state, ...payload };
